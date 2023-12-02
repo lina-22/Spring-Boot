577f2f2117e00ba9ba290d8845eb73b4062d3278
lina-22 <haquefahinur@gmail.com>
2023-12-02 23:18:48 +0100

    sp project init

---
+++ sb-backend/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+s
+Csb-backend/src/main/java/com/sb/sbbackend/SbBackendApplication.java,b\b\bb7c61e818dc2c6746c8a6972b3f2ef824465790
+B
+sb-backend/pom.xml,c\3\c3f6b4a5b23b7d652c55568104da51ca9bf70572
---
+++ sb-backend/sb-backend/src/test/java/com/sb/sbbackend/SbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.sbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
