502ef9fb33decc1f8d5838b8323e2a0a87e067c4
lina-22 <haquefahinur@gmail.com>
2023-12-05 16:25:02 +0100

    employee Model created and connected with database

--- sb-backend/sb-backend/src/main/java/com/sb/sbbackend/model/Employee.java
+++ sb-backend/sb-backend/src/main/java/com/sb/sbbackend/model/Employee.java
@@ -1,11 +1,27 @@
 package com.sb.sbbackend.model;
 
+
+import jakarta.persistence.*;
 import lombok.Data;
 
+//import javax.persistence.*;
+
+/**
+ *
+ */
 @Data
+@Entity
+@Table(name="emplyees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name="first_name", nullable = false)
     private String firstName;
+    @Column (name="last_name")
     private String lastName;
+    @Column(name="email")
     private String email;
 }
